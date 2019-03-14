06fae368acdb7e8b2ad20f21b824ec5a59e25e05
Carlos <goniti@live.fr>
2019-03-14 11:58:34 +0100

    Update insertionSort1.js

--- solutionJs/solution03/insertionSort1.js
+++ solutionJs/solution03/insertionSort1.js
@@ -2,24 +2,24 @@ const arr = [2, 4, 6, 8, 3];
 const n = arr.length;
 
 const insertionSort1 = (arr) => {
-  const last = arr[arr.length - 1]
+ const last = arr[n - 1]
 
-  for (let i = arr.length - 2; i >= 0; i--) {
+  for (let i = n - 2; i >= 0; i--) {
+    const element = arr[i];
 
-    if (i === 0 && arr[i] > last) {
-      arr[1] = arr[i]
+    if (i === 0 && element > last) {
+      arr[1] = element
       console.log(arr.join(' '));
       arr[0] = last
       console.log(arr.join(' '));
-    } else if (arr[i] > last) {
-      arr[i + 1] = arr[i]
+    } else if (element > last) {
+      arr[i + 1] = element
       console.log(arr.join(' '));
     } else {
       arr[i + 1] = last
       console.log(arr.join(' '));
       break
     }
-
   }
 }
 
