8979e50e50abf76fb0bd120f9471ad11a8a64960
Carlos <goniti@live.fr>
2019-09-12 22:45:19 +0200

    Add files via upload

---
+++ unsolved/scriptNimGame.js
@@ -0,0 +1,19 @@
+// Player  Takes           Leaving
+//                         pile=[3,2,4]
+// 1       2 from pile[1]  pile=[3,4]
+// 2       2 from pile[1]  pile=[3,2]
+// 1       1 from pile[0]  pile=[2,2]
+// 2       1 from pile[0]  pile=[1,2]
+// 1       1 from pile[1]  pile=[1,1]
+// 2       1 from pile[0]  pile=[0,1]
+// 1       1 from pile[1]  WIN
+
+
+
+const nimGame = (pile) => {
+//
+
+}
+
+let result = nimGame(pile)
+console.log(result);
