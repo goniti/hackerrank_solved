779fba983470086fd11df7556ff4dfa94c30a157
Carlos <goniti@live.fr>
2019-09-18 17:57:01 +0200

    Add files via upload

--- unsolved/Unsolved hackerrankInString/hackerrankinString.js
+++ unsolved/Unsolved hackerrankInString/hackerrankinString.js
@@ -1,76 +1,37 @@
-// hccaakkerrannkk
+//------------------------------
+////////// DON'T WORK \\\\\\\\\
+//------------------------------
+// const s = 'rhackerank' //NO
+// const s = 'hhhackkerbanker' //NO
 
-// if h => 1
-// if a => 2
-// if c => 1
-// if k => 2
-// if e => 1
-// if r => 2
-// if n => 1
-
-//return yes
-
-const s = 'hhaacckkekraraannk';
-//const s = 'rhbaasdndfsdskgbfefdbrsdfhuyatrjtcrtyytktjjt';
+//------------------------------
+//////////// WORK \\\\\\\\\\\\\\
+//------------------------------
+const s = 'hhaacckkekraraannk'; // YES
+//const s = 'rhbaasdndfsdskgbfefdbrsdfhuyatrjtcrtyytktjjt'; //NO
 
 
 const hackerrankInString = (s) => {
+    const words = 'hackerrank'
     const order = []
-    if ([...s].filter(l => l === 'h').length >= 1) {
-        order.push(s.indexOf('h'))
-        if ([...s].filter(l => l === 'a').length >= 1) {
-            order.push(s.indexOf('a'))
-            if ([...s].filter(l => l === 'c').length >= 1) {
-                order.push(s.indexOf('c'))
-                if ([...s].filter(l => l === 'k').length >= 1) {
-                    order.push(s.indexOf('k'))
-                    if ([...s].filter(l => l === 'e').length >= 1) {
-                        order.push(s.indexOf('e'))
-                        if ([...s].filter(l => l === 'r').length >= 1) {
-                            order.push(s.indexOf('r'))
-                            if ([...s].filter(l => l === 'r').length >= 1) {
-                                order.push(s.indexOf('r', (s.indexOf('r') + 1)))
-                                if ([...s].filter(l => l === 'a').length >= 1) {
-                                    order.push(s.indexOf('a', (s.indexOf('a') + 1)))
-                                    if ([...s].filter(l => l === 'n').length >= 1) {
-                                        order.push(s.indexOf('n'))
-                                        if ([...s].filter(l => l === 'k').length >= 1) {
-                                            order.push(s.indexOf('k', (s.indexOf('k') + 1)))
+    let count = 0
 
+    for (let i = 0; i < words.length; i++) {
+        order.push(s.indexOf(words[i]))
+        s = s.slice(s.indexOf(words[i]))
+    }
 
-                                            console.log(order);
-                                            console.log(order.sort((a, b) => (a > b) - (a < b)));
-
-                                            return 'YES'
-                                        } else {
-                                            return 'NO'
-                                        }
-                                    } else {
-                                        return 'NO'
-                                    }
-                                } else {
-                                    return 'NO'
-                                }
-                            } else {
-                                return 'NO'
-                            }
-                        } else {
-                            return 'NO'
-                        }
-                    } else {
-                        return 'NO'
-                    }
-                } else {
-                    return 'NO'
-                }
-            } else {
-                return 'NO'
-            }
-        } else {
-            return 'NO'
+    for (let j = 0; j < order.length; j++) {
+        if (order[j] >= 0) {
+            count++
         }
     }
 
+    if (count === order.length) {
+        return 'YES'
+    }
+
+    return 'NO'
 
 }
 
