755bd66ec4a0f1fe9b8d71c1931d50b89c4febd9
Carlos <goniti@live.fr>
2019-10-04 15:04:53 +0200

    Sequence Equation solved

---
+++ solutionJs/solution49/sequenceEquation.js
@@ -0,0 +1,46 @@
+const p = [5, 2, 1, 3, 4]
+
+const permutationEquation = (p) => {
+    const y = []
+
+    for (let i = 0; i < p.length; i++) {
+
+        y.push(p.indexOf(p.indexOf([...p].sort((a, b) => a - b)[i]) + 1) + 1)
+
+    }
+
+    return y
+}
+
+
+let result = permutationEquation(p)
+console.log(result);
+
+
+
+
+//----------------------------------------------
+// const p = [5, 2, 1, 3, 4]
+
+// const permutationEquation = (p) => {
+
+//     const n = p.length
+//     const arr = p.slice()
+//     arr.sort()
+//     const y = []
+
+//     for (let i = 0; i < n; i++) {
+//         const x = arr[i]
+//         const P = p.indexOf(x) + 1
+
+//         y.push(p.indexOf(P) + 1)
+//     }
+
+//     return y
+
+// }
+
+
+// let result = permutationEquation(p)
+// console.log(result);
+
