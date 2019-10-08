e624cbdb51f9a0e07c4b81c6095ee98e3b89a328
Carlos <goniti@live.fr>
2019-10-08 18:31:28 +0200

    unsolved

--- unsolved/unsolved pickingnumber/pickingNumber.js
+++ unsolved/unsolved pickingnumber/pickingNumber.js
@@ -4,46 +4,75 @@ const a = [4, 6, 5, 3, 3, 1]
 const n = a.length
 
 const pickingNumbers = (a) => {
-    a.sort((a, b) => a - b)
-    const arr = []
-    const arr2 = []
-    let x = 0
-    let y = 0
+    let index = 0
+    let count = 0
+    a.sort()
+    for (let i = 0; i < a.length; i++) {
+        count++
+        if (a[i] === a[0] || a[i] - 1 === a[0]) {
 
-
-    for (let i = 0; i < n; i++) {
-
-        arr.push(a[i])
-        console.log(arr);
-
-        if (Math.abs(arr[0] - arr[arr.length - 1]) <= 1) {
-            console.log(arr);
-            x++
+            index = a.indexOf(a[i], (a.indexOf(a[i]) + 1)) + 1
 
         }
 
-    }
+        if (count > a.length - 1) {
+            a.shift()
 
-
-    for (let j = n - 1; j > 0; j--) {
-
-        arr2.push(a[j])
-        console.log(arr2);
-
-        if (Math.abs(arr2[0] - arr2[arr2.length - 1]) <= 1) {
-
-            y++
-            console.log(arr2);
         }
 
+
     }
 
+    const arr = a.splice(0, index);
+    arr
+    console.log(a);
 
-    return Math.max(x, y);
 
 }
+
+
+
 
 let result = pickingNumbers(a)
 console.log(result);
 
 
+
+
+
+// a.sort((a, b) => a - b)
+// const arr = []
+// const arr2 = []
+// let x = 0
+// let y = 0
+
+
+// for (let i = 0; i < n; i++) {
+
+//     arr.push(a[i])
+//     console.log(arr);
+
+//     if (Math.abs(arr[0] - arr[arr.length - 1]) <= 1) {
+//         console.log(arr);
+//         x++
+
+//     }
+
+// }
+
+
+// for (let j = n - 1; j > 0; j--) {
+
+//     arr2.push(a[j])
+//     console.log(arr2);
+
+//     if (Math.abs(arr2[0] - arr2[arr2.length - 1]) <= 1) {
+
+//         y++
+//         console.log(arr2);
+//     }
+
+// }
+
+
+// return Math.max(x, y);
